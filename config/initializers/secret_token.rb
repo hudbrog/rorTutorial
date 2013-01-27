e620527da01f6a9228805e7f91f6c67bf90919e1
root <root@debian.localdomain>
2013-01-27 02:49:15 -0500

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorTutorial::Application.config.secret_token = '969a3cf3d99f7e3e8130792912e090e399cad3d6b9f19020be49d3dbe777320ae9b7109ef531256ad29e95ecaf910b4e466f6a2b74c663bc8eb76ee167e86336'
